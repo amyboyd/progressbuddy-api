a35fe89efbcba8150c039fa94465f035ad8bea76
adnansalehin <adnansalehin03@gmail.com>
2019-02-15 15:42:43 +0000

    Init

---
+++ src/main/kotlin/com/housingbuddy/housingbuddyapi/HousingBuddyApiApplication.kt
@@ -0,0 +1,12 @@
+package com.housingbuddy.housingbuddyapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HousingBuddyApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<HousingBuddyApiApplication>(*args)
+}
+
