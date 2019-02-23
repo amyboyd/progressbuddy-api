d766a6a28046a86deef0fee420017e22c00baae7
adnansalehin <adnansalehin03@gmail.com>
2019-02-23 23:12:54 +0000

    add endpoints for coach

--- src/main/kotlin/com/housingbuddy/housingbuddyapi/controllers/CoachController.kt
+++ src/main/kotlin/com/housingbuddy/housingbuddyapi/controllers/CoachController.kt
@@ -1,9 +1,29 @@
 package com.housingbuddy.housingbuddyapi.controllers
 
 import com.housingbuddy.housingbuddyapi.services.CoachService
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Api
 @RestController
+@RequestMapping("/coaches", produces = ["application/json"])
 class CoachController(@Autowired private val coachService: CoachService) {
+
+    @GetMapping("/{coachID}")
+    @ApiOperation("get coach")
+    fun retrieveCoach(
+        @PathVariable coachID: String
+    ) = coachService.retrieveCoachByID(coachID)
+
+    @GetMapping("/{coachID}/clients")
+    @ApiOperation("get coach events")
+    fun retrieveCoachEvents(
+        @PathVariable coachID: String
+    ) = coachService.retrieveCoachByID(coachID)?.clients
+
 }
--- src/main/kotlin/com/housingbuddy/housingbuddyapi/services/CoachService.kt
+++ src/main/kotlin/com/housingbuddy/housingbuddyapi/services/CoachService.kt
@@ -2,17 +2,24 @@ package com.housingbuddy.housingbuddyapi.services
 
 import com.housingbuddy.housingbuddyapi.models.Coach
 import com.housingbuddy.housingbuddyapi.utils.Collections
-import com.housingbuddy.housingbuddyapi.utils.Const
+import org.bson.types.ObjectId
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.mongodb.core.MongoTemplate
-import org.springframework.data.mongodb.core.query.Criteria
-import org.springframework.data.mongodb.core.query.Query
 import org.springframework.stereotype.Service
 
 @Service
 class CoachService(@Autowired private val mongoTemplate: MongoTemplate) {
-    fun retrieveCoachInfo(coachID: String): MutableList<Coach> {
-        val query = Query(Criteria(Const.ID).`is`(coachID))
-        return mongoTemplate.find(query, Coach::class.java, Collections.COACHES_COLLECTION)
+
+    fun retrieveCoachByID(coachID: String): Coach? {
+        LOGGER.info("Retrieving Coach with ID: $coachID")
+        val coach = mongoTemplate.findById(ObjectId(coachID), Coach::class.java, Collections.COACHES_COLLECTION)
+        LOGGER.info("Coach name is: ${coach?.name}")
+        return coach
+    }
+
+    private companion object {
+        val LOGGER: Logger = LoggerFactory.getLogger(CoachService::class.java)
     }
 }
